629b2c1418e3203dab373a08ac698a333f9e658c
MikaSolid <m1k4@m1k4.com>
2015-01-27 22:27:44 +0100

    Implemented MainWindow maximizing

---
+++ SmartPrinter.UI/Commands/ResizeWindowCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace SmartPrinter.UI.Commands
+{
+    public class ResizeWindowCommand : BaseCommand
+    {
+        public override void Execute(object parameter)
+        {
+            var window = Application.Current.MainWindow;
+
+            if (window.WindowState == WindowState.Maximized)
+                window.WindowState = WindowState.Normal;
+            else
+                window.WindowState = WindowState.Maximized;
+        }
+    }
+}
