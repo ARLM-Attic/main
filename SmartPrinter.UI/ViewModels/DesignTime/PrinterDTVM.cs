e2122e4ef84fd84d62977ad2f33044a5e036133b
MikaSolid <m1k4@m1k4.com>
2015-01-27 22:14:21 +0100

    Created PrinterDTVM viewmodel for design time

---
+++ SmartPrinter.UI/ViewModels/DesignTime/PrinterDTVM.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+using SmartPrint.Model.Repository;
+using SmartPrint.Model.ViewModels;
+
+namespace SmartPrinter.UI.ViewModels
+{
+    public class PrinterDTVM : PrinterVM
+    {
+        public PrinterDTVM()
+            : base(new ShellDTVM().Printers.First().Printer)
+        {
+        }
+    }
+}
